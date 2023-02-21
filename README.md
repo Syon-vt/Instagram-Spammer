42557aa478d8361df5516ade7cef8d5d7f9b8c75
Faded-exe101 <17407syon@gmail.com>
2023-02-21 12:04:34 +0400

    Build Files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Instagram Spammer
+A python applicaton for spamming messages in Instagram
+
+# Installation
+
+* ## [Download .exe File]()
+.exe file made with [Pyinstaller](https://pyinstaller.org/en/stable/index.html)
+
+* ## Run [main.py]()
+    * ### Requirments: [Instagrapi](https://adw0rd.github.io/instagrapi/)
+
+    * Edit the file and change the username and password fields
+# Thread ID
+![ThreadID](assets/url.jpg)
+As Highlighted above,
+
+the Thread ID is the number in the url of the group or DM you want to send messages to.
+# Notice: This code is in no way affiliated with, authorized, maintained, sponsored or endorsed by Instagram or any of its affiliates or subsidiaries. This is an independent and unofficial API. Use at your own risk. I will not be held responsible for any misuse of this program.
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from instagrapi import Client
+
+user = 'username'
+passwd = 'password'
+
+
+text = input("Enter Text: ")
+threadID = input("Enter thread ID: ")
+
+i = 0
+status = 0
+cl = Client()
+try:
+    cl.login(user, passwd)
+except:
+    user = input("Enter username or email: ")
+    passwd = input("Enter password: ")
+    try:
+        cl.login(user, passwd)
+    except:
+        print("Invalid username or password \n Press enter to exit")
+        input("")
+        quit()
+
+
+thread = cl.direct_threads(1)[0]
+
+
+while True:
+    if status >= 3:
+        print("Failed 3 times in a row \n Press enter to exit")
+        input("")
+        quit()
+    try:
+        cl.direct_answer(threadID, text)
+        i = i+1
+        print("Messages Sent: ", i)
+        status = 0
+    except:
+        print("Failed")
+        status = status+1
+        print("Failed attempts in a row: ", status)
